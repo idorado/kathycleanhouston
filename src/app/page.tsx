1d3f9d4cda8638455da2dd626abd6fc66ddf4896
Jose Sanchez <engineer@copydataai.com>
2025-03-06 15:12:31 +0000

    feat(app): add serviceSelector main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "@/components/sections/hero-section"
 import { ServicesSection } from "@/components/sections/services-section"
+import { ServiceSelector } from "@/components/sections/service-selector"
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <ServiceSelector />
       <ServicesSection />
     </>
   )
