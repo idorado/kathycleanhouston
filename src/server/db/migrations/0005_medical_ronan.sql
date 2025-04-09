5a94b70224969c1c26af004e2007c5bc5f77abf7
Jose Sanchez <engineer@copydataai.com>
2025-04-09 14:00:12 +0100

    refactor(db): change image, phone & email optional

---
+++ src/server/db/migrations/0005_medical_ronan.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "kathyclean_profiles" ALTER COLUMN "image" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "kathyclean_profiles" ALTER COLUMN "phone" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "kathyclean_profiles" ALTER COLUMN "email" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "kathyclean_profiles" ALTER COLUMN "address" DROP NOT NULL;
--- src/server/db/schema/profiles.ts
+++ src/server/db/schema/profiles.ts
@@ -6,10 +6,10 @@ import { createTable } from "./_table"
 export const profiles = createTable("profiles", {
     id: uuid("id").primaryKey().references(() => users.id, { onDelete: "cascade" }),
     name: text("name").notNull(),
-    image: text("image").notNull(),
-    phone: text("phone").notNull(),
-    email: text("email").notNull(),
-    address: text("address").notNull(),
+    image: text("image"),
+    phone: text("phone"),
+    email: text("email"),
+    address: text("address"),
     createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).notNull().defaultNow(),
 });
