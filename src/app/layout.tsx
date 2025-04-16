7c732c241ac9773837383f694f793636341a6b0f
Jose Sanchez <engineer@copydataai.com>
2025-04-16 19:42:46 +0100

    feat(app): change next fonts by css fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,16 @@
 import type { Metadata } from "next";
-import { Poppins, Roboto } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
 import { TopBar } from "@/components/layout/top-bar"
 import { Header } from "@/components/layout/header"
 import { Footer } from "@/components/layout/footer"
 import { Providers, PostHogProvider } from "@/components/providers"
 
-const poppins = Poppins({
-  weight: ["400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  variable: "--font-poppins",
-});
-
-const roboto = Roboto({
-  weight: ["100",  "300", "400", "500", "700", "900"],
-  subsets: ["latin"],
-  variable: "--font-roboto",
-});
 
 export const metadata: Metadata = {
   title: "Kathy Clean",
   description: "Residential and commercial cleaning services",
-  icons: [{ rel: "icon", url: "https://r2kd0cre8z.ufs.sh/f/4fYOWO5dAlomJmgQP9jvTxoXV9D2iqbpnGgyKMaCYt3F0h6r" }],
+  icons: [{ rel: "icon", url: "https://r2kd0cre8z.ufs.sh/f/4fYOWO5dAlomaYo5lyrN0W8yorQH6lnDudV4OjgIFLEC2YpM" }],
 };
 
 export default function RootLayout({
@@ -33,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(`${poppins.className} ${roboto.className}`, "antialiased")}> 
+      <body className="antialiased"> 
         <PostHogProvider>
 
         <Providers>
