199645683f1bef718c21de05ba1e2c9ef1c1be36
Jose Sanchez <engineer@copydataai.com>
2025-05-06 00:15:39 +0100

    feat(windows): change page to component

--- src/components/WindowsServiceComponent.tsx
+++ src/components/WindowsServiceComponent.tsx
@@ -1,22 +1,26 @@
+"use server";
 import HeroSection from "@/components/HeroSection";
 import WindowPriceCalculator from "@/components/WindowPriceCalculator";
 
-export default async function WindowsPage() {
+interface WindowsServiceComponentProps {
+  location: string;
+}
+
+const WindowsServiceComponent: React.FC<WindowsServiceComponentProps> = async ({ location }) => {
   return (
     <main className="flex flex-col min-h-screen">
       <HeroSection
         category="SERVICES"
-        title="Window Washing"
+        title={`Window Cleaning ${location ? `in ${location}` : ""}`}
         description="You work hard, so let us handle the cleaning. Enjoy a spotless home and spend your time on what truly matters."
         linkUrl="https://clienthub.getjobber.com/client_hubs/40b56a75-dbed-4f70-b807-d9a216f025a3/public/work_request/new?source=new_webpage"
         linkText="REQUEST A SERVICE"
         imgUrl="https://r2kd0cre8z.ufs.sh/f/4fYOWO5dAlomM7ix4wYzdDOT26oVhPrB8Uy5g9WlSQn1sNEG"
-        imgAlt="Professional window washing service"
+        imgAlt="Professional window cleaning service"
         imgWidth={400}
         imgHeight={400}
         imgPriority
       />
-
 
       {/* Main Content */}
       <section className="py-12 md:py-16">
@@ -29,7 +33,7 @@ export default async function WindowsPage() {
                 For more than a decade, we've been delivering spotless, streak-free windows to homes and businesses. Formerly known as C-Thru Cleaning Services, we now operate under Kathy Clean, continuing our commitment to excellence with the same trusted quality and expertise.
               </p>
             </div>
-            
+
             {/* Price Calculator */}
             <div>
                 <WindowPriceCalculator />
@@ -37,7 +41,8 @@ export default async function WindowsPage() {
           </div>
         </div>
       </section>
-      
     </main>
-  )
-}
+  );
+};
+
+export default WindowsServiceComponent;
